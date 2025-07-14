e62516242296182e64815e4257f694c032275b08
Andrew Jackson <andrew@codechimp.org>
2025-07-14 16:14:03 +0100

    Add discovery

---
+++ mealie/rootfs/etc/cont-init.d/91-discovery.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bashio
+declare config
+
+config=$(\
+    bashio::var.json \
+        host "$(bashio::addon.ip_address)" \
+        port "^$(bashio::addon.port 9001)" \
+)
+
+if bashio::discovery "mealie" "${config}" > /dev/null; then
+    bashio::log.info "Successfully send discovery information to Home Assistant."
+else
+    bashio::log.error "Discovery message to Home Assistant failed!"
+fi
